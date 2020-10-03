bd78115e7ee7422911a2699b9bbf37ab62ca9890
screenleon <screen.leon@gmail.com>
2020-10-04 01:17:44 +0800

    C constants and literals

---
+++ tutorial/constants_literals/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// #define LENGTH 10
+// #define WIDTH 5
+// #define NEWLINE '\n'
+
+int main() {
+    const int LENGTH = 10;
+    const int WIDTH = 5;
+    const char NEWLINE = '\n';
+    int area;
+
+    area = LENGTH * WIDTH;
+    printf("value of area : %d", area);
+    printf("%c", NEWLINE);
+
+    return 0;
+}
